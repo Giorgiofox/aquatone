3a380cd1ba55095e06103e508ff68ab69ada1f34
Giorgio Campiotti <giorgiofox@gmail.com>
2022-03-24 10:30:18 +0100

    versioning

--- core/banner.go
+++ core/banner.go
@@ -2,7 +2,7 @@ package core
 
 const (
 	Name    = "aquatone"
-	Version = "1.9.1-giorgio"
+	Version = "1.9.2b-giorgio"
 	Author  = "Giorgio Campiotti"
 	Website = "https://github.com/giorgiofox/aquatone"
 )
